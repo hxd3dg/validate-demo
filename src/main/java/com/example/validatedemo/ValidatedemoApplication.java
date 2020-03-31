4679241fda70a4d7bc0631036ae8d35abae58768
yangzhi <hzyangzhi@highstore.cn>
2020-03-31 17:42:38 +0800

    初始化

---
+++ src/main/java/com/example/validatedemo/ValidatedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.validatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidatedemoApplication.class, args);
+    }
+
+}
